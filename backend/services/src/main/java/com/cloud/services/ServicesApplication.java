3da521c0383dd304223d28f5144b897f1481220e
agoel25 <agoel25@student.ubc.ca>
2024-07-31 00:45:08 -0700

    Database connection working

--- backend/services/src/main/java/com/cloud/services/ServicesApplication.java
+++ backend/services/src/main/java/com/cloud/services/ServicesApplication.java
@@ -1,12 +1,16 @@
 package com.cloud.services;
 
+import com.cloud.services.database.DatabaseConnectionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ServicesApplication {
 
 	public static void main(String[] args) {
+		DatabaseConnectionHandler databaseConnectionHandler = new DatabaseConnectionHandler();
 		SpringApplication.run(ServicesApplication.class, args);
 	}
 
