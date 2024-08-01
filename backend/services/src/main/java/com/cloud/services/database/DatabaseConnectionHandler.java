7d5674f6819b68b730e38a86fa4eee138c39f335
ashmit03 <ashmit03@students.cs.ubc.ca>
2024-07-31 18:13:05 -0700

    Added SQL initialization script for creating tables

--- backend/services/src/main/java/com/cloud/services/database/DatabaseConnectionHandler.java
+++ backend/services/src/main/java/com/cloud/services/database/DatabaseConnectionHandler.java
@@ -3,6 +3,8 @@
 package com.cloud.services.database;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
@@ -13,8 +15,8 @@ public class DatabaseConnectionHandler {
     private Connection connection = null;
 
     final private String oracleUrl = "jdbc:oracle:thin:@localhost:1522:stu";
-    final private String username = "ora_agoel25";
-    final private String password = "a84874379";
+    final private String username = "";
+    final private String password = "";
 
     public DatabaseConnectionHandler() {
         try {
@@ -46,16 +48,212 @@ public class DatabaseConnectionHandler {
 
     public void setupDatabase() {
         dropTablesIfExists();
+        List<String> queryList = new ArrayList<String>();
 
         try {
-            String sqlQuery = "CREATE TABLE Customer (" +
+            queryList.add("CREATE TABLE Customer (" +
                     "Email VARCHAR(30) PRIMARY KEY," +
                     "Name VARCHAR(30) NOT NULL," +
                     "PhoneNumber VARCHAR(15) NOT NULL UNIQUE," +
                     "Password VARCHAR(30) NOT NULL," +
                     "Address VARCHAR(50) NOT NULL" +
-                    ")";
-            executeQuery(sqlQuery);
+                    ")");
+
+            queryList.add("CREATE TABLE Project (" +
+                    "ProjectID INTEGER PRIMARY KEY," +
+                    "Name VARCHAR(30)," +
+                    "Description VARCHAR(100)," +
+                    "CreationDate DATE NOT NULL," +
+                    "Status VARCHAR(20)" +
+                    ")");
+            queryList.add("CREATE TABLE PartOf (" +
+                    "Email VARCHAR(30)," +
+                    "ProjectID INTEGER," +
+                    "PRIMARY KEY (Email, ProjectID)," +
+                    "FOREIGN KEY (Email) REFERENCES Customer(Email)," +
+                    "FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE AddressInfo (" +
+                    "PostalCode VARCHAR(15) PRIMARY KEY," +
+                    "City VARCHAR(20) NOT NULL," +
+                    "Country VARCHAR(20) NOT NULL" +
+                    ")");
+
+            queryList.add("CREATE TABLE BillingInfo (" +
+                    "CardNumber INTEGER PRIMARY KEY," +
+                    "PaymentType VARCHAR(6) NOT NULL" +
+                    ")");
+
+            queryList.add("CREATE TABLE PaymentInfo (" +
+                    "CardNumber INTEGER," +
+                    "Email VARCHAR(30)," +
+                    "CVV INTEGER NOT NULL," +
+                    "PostalCode VARCHAR(15) NOT NULL," +
+                    "ExpiryDate DATE NOT NULL," +
+                    "IsDefault VARCHAR(5)," +
+                    "PRIMARY KEY (CardNumber, Email)," +
+                    "FOREIGN KEY (Email) REFERENCES Customer(Email)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (CardNumber) REFERENCES BillingInfo(CardNumber)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (PostalCode) REFERENCES AddressInfo(PostalCode)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE ServiceType (" +
+                    "Type VARCHAR(20) PRIMARY KEY," +
+                    "CostType VARCHAR(20) NOT NULL" +
+                    ")");
+
+
+            queryList.add("CREATE TABLE ServiceDetails (" +
+                    "Name VARCHAR(30) PRIMARY KEY," +
+                    "Type VARCHAR(20) NOT NULL," +
+                    "Description VARCHAR(100) NOT NULL," +
+                    "Status VARCHAR(20)," +
+                    "CostPerUnit INTEGER NOT NULL," +
+                    "FOREIGN KEY (Type) REFERENCES ServiceType(Type)" +
+                    ")");
+
+            queryList.add("CREATE TABLE CustomerUsesService (" +
+                    "Email VARCHAR(30)," +
+                    "Name VARCHAR(30)," +
+                    "PRIMARY KEY (Email, Name)," +
+                    "FOREIGN KEY (Email) REFERENCES Customer(Email)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (Name) REFERENCES ServiceDetails(Name)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE ProjectUsesService (" +
+                    "ProjectID INTEGER," +
+                    "Name VARCHAR(30)," +
+                    "PRIMARY KEY (ProjectID, Name)," +
+                    "FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (Name) REFERENCES ServiceDetails(Name)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE Storage (" +
+                    "Name VARCHAR(30) PRIMARY KEY," +
+                    "Capacity INTEGER," +
+                    "Latency INTEGER," +
+                    "FOREIGN KEY (Name) REFERENCES ServiceDetails(Name)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE Compute (" +
+                    "Name VARCHAR(30) PRIMARY KEY," +
+                    "CPUCores INTEGER," +
+                    "MaxMemory INTEGER," +
+                    "FOREIGN KEY (Name) REFERENCES ServiceDetails(Name)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE Functional (" +
+                    "Name VARCHAR(30) PRIMARY KEY," +
+                    "Timeout INTEGER," +
+                    "ConcurrencyLimit INTEGER," +
+                    "FOREIGN KEY (Name) REFERENCES ServiceDetails(Name)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE Region (" +
+                    "Name VARCHAR(30) PRIMARY KEY," +
+                    "Location VARCHAR(20) NOT NULL," +
+                    "Capacity INTEGER," +
+                    "Status VARCHAR(20)" +
+                    ")");
+
+            queryList.add("CREATE TABLE AvailableIn (" +
+                    "ServiceName VARCHAR(30)," +
+                    "RegionName VARCHAR(30)," +
+                    "PRIMARY KEY (ServiceName, RegionName)," +
+                    "FOREIGN KEY (ServiceName) REFERENCES ServiceDetails(Name)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (RegionName) REFERENCES Region(Name)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE ProjectSecurity (" +
+                    "ProjectID INTEGER PRIMARY KEY," +
+                    "SecurityGroupID INTEGER UNIQUE NOT NULL," +
+                    "FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE SecurityProtocol (" +
+                    "InboundProtocol VARCHAR(20) PRIMARY KEY," +
+                    "OutboundProtocol VARCHAR(20)" +
+                    ")");
+
+            queryList.add("CREATE TABLE SecurityInfo (" +
+                    "ProjectID INTEGER PRIMARY KEY," +
+                    "Name VARCHAR(30)," +
+                    "InboundProtocol VARCHAR(20)," +
+                    "CreationDate DATE," +
+                    "FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (InboundProtocol) REFERENCES SecurityProtocol(InboundProtocol)" +
+                    ")");
+
+            queryList.add("CREATE TABLE ServerTypeInfo (" +
+                    "ServerType VARCHAR(30) PRIMARY KEY," +
+                    "Memory INTEGER," +
+                    "Storage INTEGER," +
+                    "OS VARCHAR(20)," +
+                    "CPUCores INTEGER" +
+                    ")");
+
+            queryList.add("CREATE TABLE ServerInfo (" +
+                    "ServerID INTEGER PRIMARY KEY," +
+                    "Name VARCHAR(30)," +
+                    "ServerType VARCHAR(30)," +
+                    "Status VARCHAR(20)," +
+                    "Uptime INTEGER," +
+                    "CreatedAt DATE," +
+                    "UpdatedAt DATE," +
+                    "FOREIGN KEY (Name) REFERENCES Region(Name)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (ServerType) REFERENCES ServerTypeInfo(ServerType)" +
+                    "ON DELETE CASCADE" +
+                    ")");
+
+            queryList.add("CREATE TABLE Instance (" +
+                    "InstanceID INTEGER PRIMARY KEY," +
+                    "Name VARCHAR(30) NOT NULL," +
+                    "ServerID INTEGER NOT NULL," +
+                    "ProjectID INTEGER," +
+                    "Type VARCHAR(20)," +
+                    "TotalCost INTEGER," +
+                    "Status VARCHAR(20)," +
+                    "LaunchDate DATE," +
+                    "StopDate DATE," +
+                    "FOREIGN KEY (Name) REFERENCES ServiceDetails(Name)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (ServerID) REFERENCES ServerInfo(ServerID)" +
+                    "ON DELETE CASCADE," +
+                    "FOREIGN KEY (ProjectID) REFERENCES ProjectSecurity(ProjectID)" +
+                    "ON DELETE SET NULL" +
+                    ")");
+
+            queryList.add("CREATE TABLE Bills (" +
+                    "InstanceID INTEGER," +
+                    "Email VARCHAR(30)," +
+                    "CardNumber INTEGER," +
+                    "Cost INTEGER," +
+                    "PRIMARY KEY (InstanceID, Email, CardNumber)," +
+                    "FOREIGN KEY (InstanceID) REFERENCES Instance(InstanceID)," +
+                    "FOREIGN KEY (CardNumber, Email) REFERENCES PaymentInfo(CardNumber, Email)," +
+                    "FOREIGN KEY (CardNumber) REFERENCES BillingInfo(CardNumber)" +
+                    ")");
+
+            for (String query : queryList) {
+                executeQuery(query);
+            }
             System.out.println("Tables created!");
         } catch (Exception e){
             System.out.println(EXCEPTION_TAG + " " + e.getMessage());
