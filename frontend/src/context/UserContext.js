59355b1855f7e3ff028df2e23acc6fa61fc6fa44
kazuote <kazuote@students.cs.ubc.ca>
2024-08-03 15:00:19 -0700

    fixed getinstances

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -135,7 +135,7 @@ export const UserProvider = ({ children }) => {
   const getInstancesByServiceID = async (projectID, serviceName) => {
     const params = { projectID, name: serviceName };
     try {
-      const response = await axios.get("http://localhost:8080/api/getInstance", { params });
+      const response = await axios.get("http://localhost:8080/api/getInstances", { params });
       return response.data;
     } catch (error) {
       throw new Error(error.response.data);
