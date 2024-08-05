b80ccd1543356207f41448e66635c43e2f6d9566
kazuote <kazuote@students.cs.ubc.ca>
2024-08-05 11:37:40 -0700

    fixed addcard removal

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -368,7 +368,6 @@ export const UserProvider = ({ children }) => {
         signup,
         login,
         addProject,
-        addCard,
         addInstance,
         addService,
         setProjects,
